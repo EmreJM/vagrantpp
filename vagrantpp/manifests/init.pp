dbd16dcb054400b554daae41c62a7d7e96aaaea5
EmreJM <emre.olgun@myy.haaga-helia.fi>
2017-12-04 17:02:31 +0000

    Update init.pp

--- vagrantpp/manifests/init.pp
+++ vagrantpp/manifests/init.pp
@@ -1,5 +1,27 @@
 class vagrantpp {
 
+	package { 'virtualbox':
+
+		ensure => 'installed',
+		
+		allowcdrom => true,
+
+}
+	package { 'vagrant':
+
+		ensure => 'installed',
+		
+		allowcdrom => true,
+
+}
+	package { 'ssh':
+
+		ensure => 'installed',
+		
+		allowcdrom => true,
+
+}
+	
 	file { '/home/xubuntu/vagrant/Vagrantfile':
 
 		content => template('vagrantpp/Vagrantfile'),
